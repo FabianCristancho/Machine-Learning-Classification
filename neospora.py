30220723bc2a2e413873cdc8e1a0e1518e8192b7
FabianCristancho <fabiancrisrincon@outlook.com>
2021-02-09 16:57:07 -0500

    Descripcion del proceso referente al algoritmo

--- neospora.py
+++ neospora.py
@@ -1,40 +1,34 @@
-#Importaciones necesarias
-import  numpy as np
-import  pandas as pd
-import seaborn as sb
-import matplotlib.pyplot as plt
-plt.rcParams['figure.figsize'] = (16, 9)
-plt.style.use('ggplot')
-from sklearn import tree
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import  KFold
-from sklearn.model_selection import cross_val_score
-from IPython.display import Image as PImage
-from subprocess import check_call
-from subprocess import call
+import pandas as pd # Analisis y manipulación de datos
 
-from PIL import Image, ImageDraw
+import seaborn as sb # Creacion de graficos estadisticos informativos
+import pydot # Creacion de archivo descriptivo en texto plano (DOT)
+
+from sklearn import tree # Aprendizaje automatico
+from sklearn.model_selection import KFold
 
 
+# 1. Obtener datos y almacenarlos en un dataframe
 neospora = pd.read_excel('Neospora.xlsx', 'Hoja1')
 print(neospora.groupby('NEOSPORA').size())
-sb.factorplot('RAZA',data=neospora,kind="count")
+
+
+# 2. Creacion de filtros, graficos y analisis de datos en crudo
+sb.catplot(x='RAZA',data=neospora,kind="count")
 grp = neospora[['RAZA', 'NEOSPORA']].groupby(['RAZA'], as_index=True).agg(['mean', 'count'])
 print(grp)
 
-# Mapear datos del dataframe para categorizarlos
+
+# 3. Mapear datos del dataframe para categorizarlos
 # Mapping de la edad
 neospora['cat_edad'] = neospora['EDAD'].map({'> 2 AÑOS': 1,
                                             '> 3 AÑOS': 2,
                                             '> 4 AÑO': 3}).astype(int)
-
 
 # Mapping de la raza
 neospora['cat_raza'] = neospora['RAZA'].map({'AYR': 1,
                                              'HOL': 2,
                                              'NOR': 3,
                                              'JER': 4}).astype(int)
-
 
 # Mapping del toro
 neospora['cat_toro'] = neospora['TORO'].map({False: 0,
@@ -69,13 +63,14 @@ neospora['cat_muerte_embrionaria'] = neospora['MUERTE_EMBRIONARIA'].map({False: 
                                              True:1}).astype(int)
 
 
+# 4. Eliminacion de datos repetidos u obsoletos, y verificacion de los nuevos datos
 drop_elements = ['EDAD', 'RAZA', 'TORO', 'INSEMINACION', 'ABORTO', 'REPETICION', 'NO_CARGA', 'DISTOCIAS', 'TERNEROS_DEBILES', 'MUERTE_EMBRIONARIA']
 neospora_mapping = neospora.drop(drop_elements, axis=1)
-
 
 grp = neospora_mapping[['cat_raza', 'NEOSPORA']].groupby(['cat_raza'], as_index=False).agg(['count'])
 
 
+# 5. Entrenamiento de los datos para determinar el comportamiento del algoritmo
 cv = KFold(n_splits=5)
 accuracies = list()
 max_attributes = len(list(neospora_mapping))
@@ -105,7 +100,10 @@ for depth in depth_range:
 df = pd.DataFrame({'Max Depth': depth_range, "Average Accuracy": accuracies})
 df = df[['Max Depth', 'Average Accuracy']]
 print(df.to_string(index=False))
+print('Entrenamiento finalizado')
 
+
+# 6. Aplicacion del algoritmo de arbol de clasificacion por entropia
 y_train = neospora_mapping['NEOSPORA']
 x_train = neospora_mapping.drop(['NEOSPORA'], axis=1).values
 
@@ -114,10 +112,12 @@ decision_tree = tree.DecisionTreeClassifier(criterion='entropy',
                                             min_samples_leaf=2,
                                             max_depth=15,
                                             class_weight={True:1.2})
-
 decision_tree.fit(x_train, y_train)
+print('Arbol de decision implementado')
 
-with open(r"tree2.dot", 'w') as f:
+
+# 7. Almacenamiento de los resultados obtenidos a traves de un archivo .dot
+with open(r"tree.dot", 'w') as f:
     f = tree.export_graphviz(decision_tree,
                               out_file=f,
                               max_depth = 11,
@@ -126,10 +126,10 @@ with open(r"tree2.dot", 'w') as f:
                               class_names = ['No', 'N1 Neospora'],
                               rounded = True,
                               filled= True )
-import pydot
-(graph,) = pydot.graph_from_dot_file('tree2.dot')
-graph.write_png('image2.png')
-print('Terminado')
-# Convertir el archivo .dot a png para poder visualizarlo
-# check_call(['dot','-Tpng',r'tree1.dot','-o',r'tree1.png'])
-# PImage("tree1.png")
+print('Almacenamiento de datos resutantes completado')
+
+
+# 8. Generacion de arbol de decision comprensible por el ser humano
+(graph,) = pydot.graph_from_dot_file('tree.dot')
+graph.write_png('graphic_tree.png')
+print('Grafico terminado')
